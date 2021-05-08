002e247213b41f5830cb7e839ff23f0836aa96a2
kaldan007 <kaldantenzin@gmail>
2021-05-08 20:01:35 +0530

    fix(index-update): index update method implemented

--- pedurma/save_text.py
+++ pedurma/save_text.py
@@ -56,10 +56,44 @@ def update_layer(pecha_opf_path, pecha_id, vol_id, old_layers, updater):
 def update_old_layers(pecha_opf_path, pecha_id, text_obj, pecha_idx = None):
     if not pecha_idx:
         pecha_idx = yaml.safe_load((pecha_opf_path / f"{pecha_id}.opf/index.yml").read_text(encoding='utf-8'))
-    old_vols = get_old_vol(pecha_opf_path, text_obj)
+    old_vols = get_old_vol(pecha_opf_path, pecha_id, text_obj)
     new_vols = get_new_vol(old_vols, pecha_idx, text_obj)
     for (vol_id, old_vol_base), (_, new_vol_base) in zip(old_vols.items(), new_vols.items()):
         updater = Blupdate(old_vol_base, new_vol_base)
         old_layers = get_old_layers(pecha_opf_path, pecha_id, vol_id)
         update_layer(pecha_opf_path, pecha_id, vol_id, old_layers, updater)
+
+def update_other_text_index(pecha_idx, text_id, cur_vol_offset, vol_num):
+    check_flag = False
+    for text_uuid, text in pecha_idx['annotations'].items():
+        if check_flag:
+            for vol_walker, vol_span in enumerate(text['span']):
+                if vol_span['vol'] == vol_num:
+                    pecha_idx["annotations"][text_uuid]['span'][vol_walker]['start'] += cur_vol_offset
+                    pecha_idx["annotations"][text_uuid]['span'][vol_walker]['end'] += cur_vol_offset
+                elif vol_span['vol'] > vol_num:
+                    return pecha_idx
+        if text_uuid == text_id:
+            check_flag = True
+    return pecha_idx
+
+def update_index(pecha_opf_path, pecha_id, text_obj, pecha_idx=None):
+    if not pecha_idx:
+        pecha_idx = yaml.safe_load((pecha_opf_path / f"{pecha_id}.opf/index.yml").read_text(encoding='utf-8'))
+    old_vols = get_old_vol(pecha_opf_path, pecha_id, text_obj)
+    new_vols = get_new_vol(old_vols, pecha_idx, text_obj)
+    for (vol_id, old_vol_base), (_, new_vol_base) in zip(old_vols.items(), new_vols.items()):
+        check_next_text = True
+        vol_num = int(vol_id[1:])
+        cur_vol_offset = len(new_vol_base) - len(old_vol_base)
+        if cur_vol_offset != 0:
+            for vol_walker, vol_span in enumerate(pecha_idx["annotations"][text_obj.id]['span']):
+                if vol_span['vol'] == vol_num:
+                    pecha_idx["annotations"][text_obj.id]['span'][vol_walker]['end'] += cur_vol_offset
+                elif vol_span['vol'] > vol_num:
+                    check_next_text = False
+                    break
+            if check_next_text:
+                pecha_idx = update_other_text_index(pecha_idx, text_obj.id, cur_vol_offset, vol_num)
+    return pecha_idx
 
--- tests/test_save_text.py
+++ tests/test_save_text.py
@@ -98,4 +98,92 @@ def test_update_layers():
             new_layer = yaml.safe_dump(old_layer, sort_keys=False)
             expected_layer = Path(f'./tests/data/save_text/expected_layers/{layer_name}.yml').read_text(encoding='utf-8')
             assert new_layer == expected_layer
+
+def test_update_index():
+    pecha_opf_path = Path('./tests/data/save_text/old_opf/')
+    pecha_id = "P000002"
+    text_id = "D1116"
+    text_obj = Text(
+        id="259260e8e3544fc1a9a27d7dffc72df6",
+        vol_span=['v001', 'v002'],
+        pages=[
+            Page(
+                id="1a26fd08bf2b4ebb9e2d7369347e478b",
+                page_no=1,
+                content="\nཉ༄ཚོ། །རྒྱ་གར་སྐད་དུ།\nསྟ་བ་ནཱ་མ། བོད་སྐད་དུ།\nཔར་འོས་པ་བསྔགས་\n",
+                name="Page 1",
+                vol="1",
+                image_link="https://iiif.bdrc.io/bdr:I1PD95846::I1PD958460001.jpg/full/max/0/default.jpg",
+                note_ref="46d97ed3d9ca4ddabc3c413f306df03a",
+            ),
+            Page(
+                id="e8e314a7457b40348b5dd7a744004900",
+                page_no=2,
+                content="\nགཏམ་འདི་ཙམ\nའདི་ཉིད་སྨྲ་བར་\nདང་-། །ཁྱོད་མ\n",
+                name="Page 2",
+                vol="1",
+                image_link="https://iiif.bdrc.io/bdr:I1PD95846::I1PD958460002.jpg/full/max/0/default.jpg",
+                note_ref="46d97ed3d9ca4ddabc3c413f306df03a",
+            ),
+            Page(
+                id="29c64dc1fa624b42b08814ca4f3a78b4",
+                page_no=3,
+                content="\nའདོད་གང་དག་\nསྐྱབས་འགྲོ་བ།\nསྟོང་གིས་ཀྱང་།\n",
+                name="Page 3",
+                vol="1",
+                image_link="https://iiif.bdrc.io/bdr:I1PD95846::I1PD958460003.jpg/full/max/0/default.jpg",
+                note_ref="46d97ed3d9ca4ddabc3c413f306df03a",
+            ),
+            Page(
+                id="c11d8db649854c5d89ca3df22047d07b",
+                page_no=1,
+                content="\n་་༄ལོ། །རྒྱ་གར་སྐད་དུ།\nདབྱིངས་སུ་བསྟོད་པ།\nའཚལ་ལོ། །གང་ཞིག་\n",
+                name="Page 1",
+                vol="2",
+                image_link="https://iiif.bdrc.io/bdr:I1PD95847::I1PD958470001.jpg/full/max/0/default.jpg",
+                note_ref="05d117045b0c4ea5aee3aeba558e94bd",
+            ),
+            Page(
+                id="21671cb910d9486c8ba4793305c00d58",
+                page_no=2,
+                content="\nམཐོང་ངོ་། །ཕྱོགས་\nདེ་དང་དེ་ཡི་ཕྱོགས་\nཏིང་འཛིན་རྡོ་རྗེ་ཡིས\n",
+                name="Page 2",
+                vol="2",
+                image_link="https://iiif.bdrc.io/bdr:I1PD95847::I1PD958470002.jpg/full/max/0/default.jpg",
+                note_ref="05d117045b0c4ea5aee3aeba558e94bd",
+            ),
+            Page(
+                id="671dc26715434318b3d641521d4e9292",
+                page_no=3,
+                content="\nརིམ་གྱིས་སྦྱངས་\nམེད་ཉི་མ་ཟླ་བ་ཡང་།\n་རྡུལ་kལ་སོགས།\n",
+                name="Page 3",
+                vol="2",
+                image_link="https://iiif.bdrc.io/bdr:I1PD95847::I1PD958470003.jpg/full/max/0/default.jpg",
+                note_ref="05d117045b0c4ea5aee3aeba558e94bd",
+            ),
+        ],
+        notes=[
+            NotesPage(
+                id="46d97ed3d9ca4ddabc3c413f306df03a",
+                page_no=4,
+                content="\nརྒྱ་གར་གྱི་\n༢༦༤ ༧པེ་〉〉་\nབཞུགས་གོ།",
+                name="Page 4",
+                vol="1",
+                image_link="https://iiif.bdrc.io/bdr:I1PD95846::I1PD958460004.jpg/full/max/0/default.jpg",
+            ),
+            NotesPage(
+                id="05d117045b0c4ea5aee3aeba558e94bd",
+                page_no=4,
+                content="\nའབྱོར་ཆེན་པོ་དེར་\nསྡུག་བསྔལ་གྱིས་\nདེ་ཡི་སྐུ་ལས་\n",
+                name="Page 4",
+                vol="2",
+                image_link="https://iiif.bdrc.io/bdr:I1PD95847::I1PD958470004.jpg/full/max/0/default.jpg",
+            ),
+        ],
+    )
+    pecha_idx = yaml.safe_load((pecha_opf_path / f"{pecha_id}.opf/index.yml").read_text(encoding='utf-8'))
+    new_pecha_idx = update_index(pecha_opf_path, pecha_id, text_obj, pecha_idx)
+    new_pecha_idx = yaml.safe_dump(new_pecha_idx, sort_keys=False)
+    expected_idx = Path('./tests/data/save_text/expected_index.yml').read_text(encoding='utf-8')
+    assert new_pecha_idx == expected_idx
     
