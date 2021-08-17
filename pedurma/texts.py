78d34032c288b64c0f58e0979371449eb11318a2
kaldan007 <kaldantenzin@gmail>
2021-08-17 17:26:30 +0530

    fix(text): extra line rm for better readibility

--- pedurma/texts.py
+++ pedurma/texts.py
@@ -142,6 +142,7 @@ def get_clean_page(page):
     base_page = page
     for ann, ann_pat in pat_list.items():
         base_page = re.sub(ann_pat, "", base_page)
+    base_page = base_page.lstrip()
     return base_page
 
 
@@ -152,7 +153,7 @@ def get_page_obj(page, text_meta, tag, pagination_layer):
     pg_num = get_page_num(page_idx)
     page_link = get_link(pg_num, text_meta)
     note_ref = get_note_ref(pagination)
-    if page_content == "\n":
+    if page_content == "":
         page_obj = None
     else:
         if tag == "note":
@@ -215,9 +216,12 @@ def serialize_text_obj(text):
     pages = text.pages
     notes = text.notes
     for page in pages:
-        text_hfml[f"v{int(page.vol):03}"] += page.content
+        if page.page_no != 1:
+            text_hfml[f"v{int(page.vol):03}"] += f'\n{page.content}'
+        else:
+            text_hfml[f"v{int(page.vol):03}"] += f'{page.content}'
     for note in notes:
-        text_hfml[f"v{int(note.vol):03}"] += note.content
+        text_hfml[f"v{int(note.vol):03}"] += f'\n{note.content}'
     return text_hfml
 
 def get_durchen_page_obj(page, notes):
--- tests/test_save_text.py
+++ tests/test_save_text.py
@@ -17,7 +17,7 @@ def test_update_base():
             Page(
                 id="3373e79434004aaeb8b2e69649243d2a",
                 page_no=5,
-                content="\nངོས་ལྗོན་ཤིང་\nལེན་པ་པོ་ཕུན་སུམ་ཚོགས་པའོ།\nའདི་དག་གིས་ནི་སྦྱིན་པར་\n",
+                content="ངོས་ལྗོན་ཤིང་\nལེན་པ་པོ་ཕུན་སུམ་ཚོགས་པའོ།\nའདི་དག་གིས་ནི་སྦྱིན་པར་\n",
                 name="Page 5",
                 vol="2",
                 image_link="https://iiif.bdrc.io/bdr:I1PD95847::I1PD958470005.jpg/full/max/0/default.jpg",
@@ -26,7 +26,7 @@ def test_update_base():
             Page(
                 id="71dff610d4c841c58e9c815582bf8508",
                 page_no=6,
-                content="\nམངའ་དབང་མཛད་པ་\nའདི་དག་གིས་ནི་དེའི་\nགིས་ནི་སྐྱེ་kkབ་ལ་\n",
+                content="མངའ་དབང་མཛད་པ་\nའདི་དག་གིས་ནི་དེའི་\nགིས་ནི་སྐྱེ་kkབ་ལ་\n",
                 name="Page 6",
                 vol="2",
                 image_link="https://iiif.bdrc.io/bdr:I1PD95847::I1PD958470006.jpg/full/max/0/default.jpg",
@@ -37,7 +37,7 @@ def test_update_base():
             NotesPage(
                 id="9efa117a2b9444ac8cb09c198d21cdd8",
                 page_no=7,
-                content="\nདེ་ལ་ནམ་མཁའི་\nབ་ཡང་དག་པར་\nགིས་ནི་ཆོས་སྟོན་པའི་",
+                content="དེ་ལ་ནམ་མཁའི་\nབ་ཡང་དག་པར་\nགིས་ནི་ཆོས་སྟོན་པའི་",
                 name="Page 7",
                 vol="2",
                 image_link="https://iiif.bdrc.io/bdr:I1PD95847::I1PD958470007.jpg/full/max/0/default.jpg",
@@ -61,7 +61,7 @@ def test_update_layers():
             Page(
                 id="3373e79434004aaeb8b2e69649243d2a",
                 page_no=5,
-                content="\nངོས་ལྗོན་ཤིང་\nལེན་པ་པོ་ཕུན་སུམ་ཚོགས་པའོ།\nའདི་དག་གིས་ནི་སྦྱིན་པར་\n",
+                content="ངོས་ལྗོན་ཤིང་\nལེན་པ་པོ་ཕུན་སུམ་ཚོགས་པའོ།\nའདི་དག་གིས་ནི་སྦྱིན་པར་\n",
                 name="Page 5",
                 vol="2",
                 image_link="https://iiif.bdrc.io/bdr:I1PD95847::I1PD958470005.jpg/full/max/0/default.jpg",
@@ -70,7 +70,7 @@ def test_update_layers():
             Page(
                 id="71dff610d4c841c58e9c815582bf8508",
                 page_no=6,
-                content="\nམངའ་དབང་མཛད་པ་\nའདི་དག་གིས་ནི་དེའི་\nགིས་ནི་སྐྱེ་kkབ་ལ་\n",
+                content="མངའ་དབང་མཛད་པ་\nའདི་དག་གིས་ནི་དེའི་\nགིས་ནི་སྐྱེ་kkབ་ལ་\n",
                 name="Page 6",
                 vol="2",
                 image_link="https://iiif.bdrc.io/bdr:I1PD95847::I1PD958470006.jpg/full/max/0/default.jpg",
@@ -81,7 +81,7 @@ def test_update_layers():
             NotesPage(
                 id="9efa117a2b9444ac8cb09c198d21cdd8",
                 page_no=7,
-                content="\nདེ་ལ་ནམ་མཁའི་\nབ་ཡང་དག་པར་\nགིས་ནི་ཆོས་སྟོན་པའི་",
+                content="དེ་ལ་ནམ་མཁའི་\nབ་ཡང་དག་པར་\nགིས་ནི་ཆོས་སྟོན་པའི་",
                 name="Page 7",
                 vol="2",
                 image_link="https://iiif.bdrc.io/bdr:I1PD95847::I1PD958470007.jpg/full/max/0/default.jpg",
@@ -111,7 +111,7 @@ def test_update_index():
             Page(
                 id="1a26fd08bf2b4ebb9e2d7369347e478b",
                 page_no=1,
-                content="\nཉ༄ཚོ། །རྒྱ་གར་སྐད་དུ།\nསྟ་བ་ནཱ་མ། བོད་སྐད་དུ།\nཔར་འོས་པ་བསྔགས་\n",
+                content="ཉ༄ཚོ། །རྒྱ་གར་སྐད་དུ།\nསྟ་བ་ནཱ་མ། བོད་སྐད་དུ།\nཔར་འོས་པ་བསྔགས་\n",
                 name="Page 1",
                 vol="1",
                 image_link="https://iiif.bdrc.io/bdr:I1PD95846::I1PD958460001.jpg/full/max/0/default.jpg",
@@ -120,7 +120,7 @@ def test_update_index():
             Page(
                 id="e8e314a7457b40348b5dd7a744004900",
                 page_no=2,
-                content="\nགཏམ་འདི་ཙམ\nའདི་ཉིད་སྨྲ་བར་\nདང་-། །ཁྱོད་མ\n",
+                content="གཏམ་འདི་ཙམ\nའདི་ཉིད་སྨྲ་བར་\nདང་-། །ཁྱོད་མ\n",
                 name="Page 2",
                 vol="1",
                 image_link="https://iiif.bdrc.io/bdr:I1PD95846::I1PD958460002.jpg/full/max/0/default.jpg",
@@ -129,7 +129,7 @@ def test_update_index():
             Page(
                 id="29c64dc1fa624b42b08814ca4f3a78b4",
                 page_no=3,
-                content="\nའདོད་གང་དག་\nསྐྱབས་འགྲོ་བ།\nསྟོང་གིས་ཀྱང་།\n",
+                content="འདོད་གང་དག་\nསྐྱབས་འགྲོ་བ།\nསྟོང་གིས་ཀྱང་།\n",
                 name="Page 3",
                 vol="1",
                 image_link="https://iiif.bdrc.io/bdr:I1PD95846::I1PD958460003.jpg/full/max/0/default.jpg",
@@ -138,7 +138,7 @@ def test_update_index():
             Page(
                 id="c11d8db649854c5d89ca3df22047d07b",
                 page_no=1,
-                content="\n་་༄ལོ། །རྒྱ་གར་སྐད་དུ།\nདབྱིངས་སུ་བསྟོད་པ།\nའཚལ་ལོ། །གང་ཞིག་\n",
+                content="་་༄ལོ། །རྒྱ་གར་སྐད་དུ།\nདབྱིངས་སུ་བསྟོད་པ།\nའཚལ་ལོ། །གང་ཞིག་\n",
                 name="Page 1",
                 vol="2",
                 image_link="https://iiif.bdrc.io/bdr:I1PD95847::I1PD958470001.jpg/full/max/0/default.jpg",
@@ -147,7 +147,7 @@ def test_update_index():
             Page(
                 id="21671cb910d9486c8ba4793305c00d58",
                 page_no=2,
-                content="\nམཐོང་ངོ་། །ཕྱོགས་\nདེ་དང་དེ་ཡི་ཕྱོགས་\nཏིང་འཛིན་རྡོ་རྗེ་ཡིས\n",
+                content="མཐོང་ངོ་། །ཕྱོགས་\nདེ་དང་དེ་ཡི་ཕྱོགས་\nཏིང་འཛིན་རྡོ་རྗེ་ཡིས\n",
                 name="Page 2",
                 vol="2",
                 image_link="https://iiif.bdrc.io/bdr:I1PD95847::I1PD958470002.jpg/full/max/0/default.jpg",
@@ -156,7 +156,7 @@ def test_update_index():
             Page(
                 id="671dc26715434318b3d641521d4e9292",
                 page_no=3,
-                content="\nརིམ་གྱིས་སྦྱངས་\nམེད་ཉི་མ་ཟླ་བ་ཡང་།\n་རྡུལ་kལ་སོགས།\n",
+                content="རིམ་གྱིས་སྦྱངས་\nམེད་ཉི་མ་ཟླ་བ་ཡང་།\n་རྡུལ་kལ་སོགས།\n",
                 name="Page 3",
                 vol="2",
                 image_link="https://iiif.bdrc.io/bdr:I1PD95847::I1PD958470003.jpg/full/max/0/default.jpg",
@@ -167,7 +167,7 @@ def test_update_index():
             NotesPage(
                 id="46d97ed3d9ca4ddabc3c413f306df03a",
                 page_no=4,
-                content="\nརྒྱ་གར་གྱི་\n༢༦༤ ༧པེ་〉〉་\nབཞུགས་གོ།",
+                content="རྒྱ་གར་གྱི་\n༢༦༤ ༧པེ་〉〉་\nབཞུགས་གོ།",
                 name="Page 4",
                 vol="1",
                 image_link="https://iiif.bdrc.io/bdr:I1PD95846::I1PD958460004.jpg/full/max/0/default.jpg",
@@ -175,7 +175,7 @@ def test_update_index():
             NotesPage(
                 id="05d117045b0c4ea5aee3aeba558e94bd",
                 page_no=4,
-                content="\nའབྱོར་ཆེན་པོ་དེར་\nསྡུག་བསྔལ་གྱིས་\nདེ་ཡི་སྐུ་ལས་\n",
+                content="འབྱོར་ཆེན་པོ་དེར་\nསྡུག་བསྔལ་གྱིས་\nདེ་ཡི་སྐུ་ལས་\n",
                 name="Page 4",
                 vol="2",
                 image_link="https://iiif.bdrc.io/bdr:I1PD95847::I1PD958470004.jpg/full/max/0/default.jpg",
@@ -195,7 +195,7 @@ def test_integration():
     pecha_id = "P0003"
     text_id = "D1118"
     text_obj = get_text_obj(pecha_id, text_id, pecha_opf_path)
-    new_last_page = "\nརིམ་གྱིས་སྦྱངས་\nམེད་ཉི་མ་ཟླ་བ་ཡང་།\n་རྡུལ་kkལ་སོགས།\n"
+    new_last_page = "རིམ་གྱིས་སྦྱངས་\nམེད་ཉི་མ་ཟླ་བ་ཡང་།\n་རྡུལ་kkལ་སོགས།"
     text_obj.pages[-1].content = new_last_page
     pecha_opf_path = save_text(pecha_id, text_obj, pecha_opf_path)
     new_text_obj = get_text_obj(pecha_id, text_id, pecha_opf_path)
