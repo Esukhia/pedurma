455116ff6816bcec0b2964f4768919c1e6e46073
kaldan007 <kaldantenzin@gmail>
2021-12-23 17:06:14 +0530

    fix(preview): face function of preview text updated

--- pedurma/text_report.py
+++ pedurma/text_report.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 from openpecha.utils import load_yaml
 
+from pedurma.texts import get_pecha_paths
+
 
 def get_metadata(pecha_path):
     pecha_id = Path(pecha_path).stem
@@ -39,6 +41,8 @@ def get_text_report(text_id, pecha_paths, preview_text):
         "total_number_of_footnotes": None,
         "download_date": None,
     }
+    if pecha_paths is None:
+        pecha_paths = get_pecha_paths(text_id)
     text_report["title"] = get_text_title(pecha_paths["google"])
     number_of_pages = 0
     number_of_footnotes = 0
