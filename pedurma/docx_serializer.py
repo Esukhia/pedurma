2dbeaca27abe017cb9768570ccce2c23dba18977
kaldan007 <kaldantenzin@gmail>
2021-12-28 15:42:54 +0530

    fix(preview): preview face function updated

--- pedurma/docx_serializer.py
+++ pedurma/docx_serializer.py
@@ -21,13 +21,14 @@ def create_docx_with_footnotes(text_id, collated_text, path):
 
     for chunk in chunks:
         if chunk and "<" in chunk:
-            sub_text = p.add_run(chunk)
-            sub_text.font.subscript = True
-            sub_text.font.name = "Jomolhari"
+            chunk = re.sub(r"\(\d+\) ", "", chunk)
+            super_text = p.add_run(chunk)
+            super_text.font.superscript = True
+            super_text.font.name = "Jomolhari"
         else:
             normal_text = p.add_run(chunk)
             normal_text.font.name = "Jomolhari"
-    output_path = path / f"{text_id}.docx"
+    output_path = path / f"{text_id}_format_01.docx"
     document.save(str(output_path))
     return output_path
 
@@ -76,7 +77,7 @@ def creat_docx_footnotes_at_end_of_page(text_id, collated_text, path):
     pages = get_pages(collated_text)
     for page in pages:
         document = parse_page(page, document)
-    output_path = path / f"{text_id}.docx"
+    output_path = path / f"{text_id}_format_02.docx"
     document.save(str(output_path))
     return output_path
 
