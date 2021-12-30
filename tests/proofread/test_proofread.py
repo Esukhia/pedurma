cbc8355d080a59764091bbf5cc7f9e6892c64ce9
kaldan007 <kaldantenzin@gmail>
2021-12-30 11:08:03 +0530

    fix(note_page): note page return in list

--- tests/proofread/test_proofread.py
+++ tests/proofread/test_proofread.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from pedurma.pecha import ProofreadNotePage
-from pedurma.proofreading import get_note_page
+from pedurma.proofreading import get_note_page, get_note_pages
 
 
 def test_get_note_page():
@@ -14,6 +14,22 @@ def test_get_note_page():
         page_num=720,
     )
 
-    proofread_page = get_note_page(text_id, repo_path=repo_path)
+    proofread_page = get_note_page(text_id, cur_pg_num=720, repo_path=repo_path)
 
     assert expected_page == proofread_page
+
+
+def test_get_note_pages():
+    text_id = "D1119"
+    repo_path = Path(__file__).parent / "data" / "note_page"
+    expected_note_pages = [
+        ProofreadNotePage(
+            manual="manual_note",
+            google="google_note",
+            img_link="https://iiif.bdrc.io/bdr:I1PD95846::I1PD958460720.jpg/full/max/0/default.jpg",
+            page_num=720,
+        )
+    ]
+
+    note_pages = get_note_pages(text_id, repo_path)
+    assert expected_note_pages == note_pages
